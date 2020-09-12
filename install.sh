3f3a20962ca722c9395f7ef6b7e068ca60e4d33c
zhanwang <zhanwang@inspur.com>
2020-09-12 19:18:18 +0800

    初步完成 tiny-k8s 集群搭建

--- install.sh
+++ install.sh
@@ -6,14 +6,18 @@ kubectl apply -f sources/etcd-operator.yml
 printf "==> tiny-k8s: %s\n" "create etcd cluster"
 kubectl apply -f sources/etcd-cluster.yml
 
-printf "==> tiny-k8s: %s\n" "create cert from cert-file"
+printf "==> tiny-k8s: %s\n" "create certs for kubernetes"
+kubeadm init phase certs all --config kubernetes/kubeadm.conf
 kubectl create cm tiny-certs   -n tiny-k8s --from-file kubernetes/pki/
 
-printf "==> tiny-k8s: %s\n" "create cert from cert-file"
-kubectl create cm tiny-configs -n tiny-k8s --from-file kubernetes/
-
-printf "%s\n" "证书自动签名"
-kubectl2 create clusterrolebinding kubelet-bootstrap --clusterrole=system:node-bootstrapper --user=kubelet-bootstrap
+printf "==> tiny-k8s: %s\n" "create kubeconfig for admin controller-manager scheduler"
+kubeadm init phase kubeconfig all --config kubernetes/kubeadm.conf --kubeconfig-dir /root/zhanwang/tiny-k8s/kubernetes/kubeconfig
+kubectl create cm tiny-configs -n tiny-k8s --from-file kubernetes/kubeconfig/
 
 printf "%s\n" "创建 cluster-info 为 node 节点加入"
 kubectl2 create -f sources/cluster-info.yml
+
+
+### clean unuseful file
+rm -rf /root/zhanwang/tiny-k8s/kubernetes/kubeconfig/kubelet.conf
+rm -rf kubernetes/pki/etcd/ kubernetes/pki/apiserver-etcd-client.*
