a48eef1f22ae2f83ae64ce2489645882ff019d07
zhanwang <zhanwang@inspur.com>
2020-09-14 11:01:15 +0800

    kubeadm join 配置示例

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ kubeadm init phase kubeconfig all --config kubernetes/kubeadm.conf --kubeconfig-
 kubectl create cm tiny-configs -n tiny-k8s --from-file kubernetes/kubeconfig/
 
 printf "%s\n" "tiny-k8s control-plane init"
-kubectl create -f sources/control-plane.yml
+kubectl apply -f sources/control-plane.yml
 
 printf "%s\n" "create kube-proxy, dns plugin, network plugin for tiny-k8s"
-kubectl2 create -f sources/plugins.yml
+kubectl2 apply -f sources/plugins.yml
 
 printf "%s\n" "create cluster-info for kubeadm join node"
-kubectl2 create -f sources/cluster-info.yml
+kubectl2 apply -f sources/cluster-info.yml
 
 
 ### join node into tiny k8s
 # kubeadm token create
-# kubeadm join xxx:30443 --token xxxx
+# kubeadm join 10.110.18.78:30443 --token zb98xj.j1pa1edetukt75xp --discovery-token-ca-cert-hash sha256:dc079e11dbae14e70ab098b29471df366085ae80091573e3e8152b7fb48c7eac -v=8
 
 
 ### clean unuseful file
